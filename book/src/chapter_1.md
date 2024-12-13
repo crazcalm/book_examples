54744a16605acd6ce9ddc55eae3cfcc4b9b26d22
Marcus Willock <crazcalm@gmail.com>
2024-12-13 03:31:16 -0500

    Initial commit for the mdbook stuff

---
+++ book/src/chapter_1.md
@@ -0,0 +1,13 @@
+# Chapter 1
+hi
+
+```rust,editable
+fn main() {
+    let number = 5;
+    print!("{}", number);
+}
+```
+
+```rust
+{{#include ../../examples/example1-1.rs:example}}
+```
--- examples/example1-1.rs
+++ examples/example1-1.rs
@@ -1,4 +1,12 @@
-fn main() {
-    // Uncomment to run example
-    //println!("{} > {} = {}", 1, 'a', 1 > 'a');
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    #[should_panic]
+    fn test_add() {
+        // ANCHOR: example
+        println!("{} > {} = {}", 1, 'a', 1 > 'a');
+        // ANCHOR_END: example
+    }
 }
